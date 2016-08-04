55376c94edeb09cf67b786b5d385e8ca034bf8d3
Shawn Inder <shawn@vigour.io>
2016-08-03 22:06:16 -0400

    Ported from vigour-util + added options object and object mode and shortcut syntax

---
+++ README.md
@@ -0,0 +1,49 @@
+# unflatten
+
+<!-- VDOC.badges travis; standard; npm; coveralls -->
+<!-- DON'T EDIT THIS SECTION (including comments), INSTEAD RE-RUN `vdoc` TO UPDATE -->
+[![Build Status](https://travis-ci.org/vigour-io/unflatten.svg?branch=master)](https://travis-ci.org/vigour-io/unflatten)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+[![npm version](https://badge.fury.io/js/unflatten.svg)](https://badge.fury.io/js/unflatten)
+[![Coverage Status](https://coveralls.io/repos/github/vigour-io/unflatten/badge.svg?branch=master)](https://coveralls.io/github/vigour-io/unflatten?branch=master)
+
+<!-- VDOC END -->
+
+<!-- VDOC.jsdoc unflatten -->
+<!-- DON'T EDIT THIS SECTION (including comments), INSTEAD RE-RUN `vdoc` TO UPDATE -->
+#### var obj = unflatten(subject, [opts])
+
+Opposite of [`flatten`](#flatten). Unflattens an object with delimited keys
+- **subject** (*object*) - Object that needs to be unflattened
+- **[opts]** (*object|string|boolean*) - Optional.
+
+- Provide a string as a shortcut for `{ separator: opts }`
+
+- Provide a boolean as a shorcut for `{ objectMode: opts }`
+
+- Provide an object to set both options `{ separator: '/', objectMode: true }`
+
+- Available options:
+
++ **separator** (*string*) - defaults to `'.'`
+
++ **objectMode** (*boolean*) - defaults to `false`
+- **return** (*object*) obj - Nested Javascript object
+
+<!-- VDOC END -->
+
+```javascript
+var unflatten = require('unflatten')
+unflatten({
+  'a.b.c': 'd'
+})
+/*
+{
+  a: {
+    b: {
+      c: 'd'
+    }
+  }
+}
+*/
+```
---
+++ test/index.js
@@ -0,0 +1,91 @@
+'use strict'
+
+var test = require('tape')
+var unflatten = require('../')
+
+var testCases = {
+  separator: '/',
+  plain: [
+    // ['object', unflattened]
+    [{ 'a.b.c': 'd' }, { a: { b: { c: 'd' } } }],
+    [{ 'a.b.c': 'd', 'a.b.e': 'f' }, { a: { b: { c: 'd', e: 'f' } } }],
+    [{ a: 'b' }, { a: 'b' }],
+    [{ 'a.0': 'hello', 'a.1': 'world' }, { a: ['hello', 'world'] }]
+  ],
+  withSeparator: [
+    // ['object', unflattened]
+    [{ 'a/b/c': 'd' }, { a: { b: { c: 'd' } } }],
+    [{ 'a/b/c': 'd', e: 'f' }, { a: { b: { c: 'd' } }, e: 'f' }],
+    [{ 'a/0': 'hello', 'a/1': 'world' }, { a: ['hello', 'world'] }]
+  ],
+  objectModeOff: [
+    // ['object', unflattened]
+    [{ 'a.0': 'hello', 'a.1': 'world' }, { a: ['hello', 'world'] }]
+  ],
+  objectModeOn: [
+    // ['object', unflattened]
+    [{ 'a.0': 'hello', 'a.1': 'world' }, { a: { 0: 'hello', 1: 'world' } }]
+  ]
+}
+
+test('unflatten with no arguments', function (t) {
+  t.plan(testCases.plain.length)
+  testCases.plain.forEach(function (item) {
+    t.deepEqual(unflatten(item[0]),
+      item[1],
+      'unflatten(' + JSON.stringify(item[0]) + ') === ' + JSON.stringify(item[1]))
+  })
+})
+
+test('unflatten with a different separator', function (t) {
+  t.plan(testCases.withSeparator.length)
+  testCases.withSeparator.forEach(function (item) {
+    t.deepEqual(unflatten(item[0], { separator: testCases.separator }),
+      item[1],
+      'unflatten(' + JSON.stringify(item[0]) + ', { separator: \'' +
+        testCases.separator + '\' }) === ' + JSON.stringify(item[1]))
+  })
+})
+
+test('separator shortcut', function (t) {
+  t.plan(testCases.withSeparator.length)
+  testCases.withSeparator.forEach(function (item) {
+    t.deepEqual(unflatten(item[0], testCases.separator),
+      item[1],
+      'unflatten(' + JSON.stringify(item[0]) + ', \'' +
+        testCases.separator + '\') === ' + JSON.stringify(item[1]))
+  })
+})
+
+test('unflatten with object mode off', function (t) {
+  t.plan(testCases.objectModeOff.length * 2)
+  testCases.objectModeOff.forEach(function (item) {
+    var unflattened = unflatten(item[0])
+    t.deepEqual(unflattened,
+      item[1],
+      'unflatten(' + JSON.stringify(item[0]) + ') === ' + JSON.stringify(item[1]))
+    t.equal(unflattened.a instanceof Array, true, 'converts array-like things to arrays')
+  })
+})
+
+test('unflatten with object mode on', function (t) {
+  t.plan(testCases.objectModeOn.length * 2)
+  testCases.objectModeOn.forEach(function (item) {
+    var unflattened = unflatten(item[0], { objectMode: true })
+    t.deepEqual(unflattened,
+      item[1],
+      'unflatten(' + JSON.stringify(item[0]) + ', { objectMode: true }) === ' + JSON.stringify(item[1]))
+    t.equal(unflattened.a instanceof Array, false, 'converts array-like things to objets')
+  })
+})
+
+test('object mode shortcut', function (t) {
+  t.plan(testCases.objectModeOn.length * 2)
+  testCases.objectModeOn.forEach(function (item) {
+    var unflattened = unflatten(item[0], true)
+    t.deepEqual(unflattened,
+      item[1],
+      'unflatten(' + JSON.stringify(item[0]) + ', true) === ' + JSON.stringify(item[1]))
+    t.equal(unflattened.a instanceof Array, false, 'converts array-like things to objets')
+  })
+})
